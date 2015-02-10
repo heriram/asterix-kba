34a170ab9fe03fd12e0e24e43fde913f666e634f
Heri Ramampiaro <heri@ntnu.no>
2015-02-10 11:57:48 -0800

    Bug fixes

--- src/main/java/edu/uci/ics/asterix/external/library/utils/StringUtil.java
+++ src/main/java/edu/uci/ics/asterix/external/library/utils/StringUtil.java
@@ -7,7 +7,7 @@ import java.util.regex.Pattern;
 
 public class StringUtil {
     public static final String SPECIAL_CHARACTERS = "`~!#$%^*()_+[\\];',./{}|:\"<>?";
-    
+
     public static String cleanUp(String s) {
         final String specialChars2 = "[" + Pattern.quote(SPECIAL_CHARACTERS) + "]+";
 
@@ -16,104 +16,102 @@ public class StringUtil {
         s = s.replaceAll(specialChars2, " ");
         return s;
     }
-    
+
     public static String[] wrapString(String str, final int maxLen) {
         int len = str.length();
-        int arrayLen = 1 + (int)len/maxLen;
-        
+        int arrayLen = 1 + (int) len / maxLen;
+
         if (len < maxLen)
-            return new String[]{str};
-        
+            return new String[] { str };
+
         String strArray[] = new String[arrayLen];
         CharBuffer strBuff = CharBuffer.wrap(str);
-  
+
         int remaining = 0;
         char b[] = null;
-        int i=0;
+        int i = 0;
         while ((remaining = strBuff.remaining()) > maxLen) {
             b = new char[maxLen];
             strBuff.get(b);
-            strArray[i++] = b.toString();
+            strArray[i] = new String(b);
+            i++;
         }
         b = new char[remaining];
         strBuff.get(b);
-        strArray[i] = b.toString();
+        strArray[i] = new String(b);
 
         return strArray;
 
     }
-    
+
     public static int lastIndexOf(String str, int beginIndex, int endIndex, char character) {
-        int index = endIndex-1;
-        for (; index>beginIndex; index--) {
-            char c  = str.charAt(index);
-            if (c==character)
+        final char[] strChars = str.toCharArray();
+        int index = endIndex;
+        int fromIndex = beginIndex >= 0 ? beginIndex : 0;
+        for (; index > fromIndex; index--) {
+            char c = strChars[index];
+            if (c == character)
                 return index;
         }
-        return -1;
-        
+
+        // character was not found
+        return endIndex;
+
     }
-    
+
     public static int lastIndexOf(String str, int endIndex, char character) {
         return lastIndexOf(str, 0, endIndex, character);
     }
-    
-    
-    
+
     public static String[] breakString(String str, final int maxLen) {
         int len = str.length();
         List<String> subStrings = new ArrayList<String>();
-        
-        if (len < maxLen)
-            return new String[]{str};
-        
-        
+
+        if (len <= maxLen)
+            return new String[] { str };
+
         int beginIndex = 0;
-        int endIndex = maxLen;         
-        while(endIndex<=len) {
-            endIndex = lastIndexOf(str, beginIndex, endIndex, ' ');
-            subStrings.add(str.substring(beginIndex, endIndex)+'\n');
-            beginIndex = ++endIndex;
-            endIndex = beginIndex + maxLen; 
+        int endIndex = maxLen - 1;
+        while (endIndex < len) {
+            endIndex = Math.min(lastIndexOf(str, beginIndex, endIndex, ' '), len - 1);
+            subStrings.add(str.substring(beginIndex, endIndex) + '\n');
+            beginIndex = endIndex + 1;
+            endIndex = beginIndex + maxLen - 2;
+
         }
-        
+
         subStrings.add(str.substring(beginIndex, len));
-        
+
         return subStrings.toArray(new String[subStrings.size()]);
     }
-    
-    
-    public static String[] tokenize(String string, char delimiter) 
-    {
+
+    public static String[] tokenize(String string, char delimiter) {
         ThreadLocal<String[]> tempArray = new ThreadLocal<String[]>();
         String[] temp = tempArray.get();
         int tempLength = (string.length() / 2) + 2;
- 
-        if (temp == null || temp.length < tempLength) 
-        {
+
+        if (temp == null || temp.length < tempLength) {
             temp = new String[tempLength];
             tempArray.set(temp);
         }
- 
- 
+
         int wordCount = 0;
         int i = 0;
         int j = string.indexOf(delimiter);
- 
-        while (j >= 0)
-        {
+
+        while (j >= 0) {
             String word = string.substring(i, j).trim();
             if (!word.isEmpty())
                 temp[wordCount++] = word;
             i = j + 1;
             j = string.indexOf(delimiter, i);
         }
- 
+
         temp[wordCount++] = string.substring(i);
- 
+
         String[] result = new String[wordCount];
         System.arraycopy(temp, 0, result, 0, wordCount);
         return result;
     }
-    
+
 }
