6d13f89aa89a03be4684a1b5ef0f253ba5517c90
Heri Ramampiaro <heri@ntnu.no>
2015-06-11 22:50:27 -0700

    Bug fix - increasing portability

--- src/main/resources/reinstall_asterix.sh
+++ src/main/resources/reinstall_asterix.sh
@@ -7,11 +7,15 @@ usage: $0 [restart|stop] options
 This script run re-install a new copy of Asterix (managix)
 
 OPTIONS:
-   -o      	Optional - run maven offline to avoid downloading "xml" metadata files (same as mvn -o)
-   -c|--clean   Optional - run clean before building the package
-   -m|--managixdir   Managix directory
-   -a|-- asterixsrc   Asterix source code directory
+   -o               Optional - run maven offline to avoid downloading "xml" metadata files (same as mvn -o)
+   -c|--clean       Optional - run clean before building the package
+   -m|--managixdir  Managix directory (default: ${MANAGIX_HOME})
+   -a|--asterixsrc  Asterix source code directory
    -d|--asterixdev  Home directory for external feed adapters and UDF development
+   -v|--version     Asterix version (default: ${VERSION})
+
+   Example: ./reinstall_asterix.sh -m /Users/heri/asterix-master-mgmt -s
+         /Users/heri/Work/Workspace/incubator-asterixdb -d /Users/heri/git/asterix-feed -o -v 0.8.7
 EOF
 }
 
@@ -19,6 +23,7 @@ ASTERIX_SOURCE=/Users/heri/Work/Workspace/Experimental/asterix_feeds/asterixdb
 ASTERIX_EXTERNAL_DEV_HOME=/Users/heri/git/asterix-feed
 ONLINE=
 CLEAN=
+VERSION=0.8.6
 
 while [[ $# > 0 ]]
 do
@@ -36,6 +41,15 @@ case $key in
     -m|--managixdir)
     MANAGIX_HOME="$2"
     if [ -z $MANAGIX_HOME ]
+    then
+    	usage
+    	exit 1
+    fi
+    shift # past argument
+    ;;
+    -v|--version)
+    VERSION="$2"
+    if [ -z $VERSION ]
     then
     	usage
     	exit 1
@@ -77,6 +91,8 @@ ASTERIX_EXTERNAL_DEV_RESOURCES=${ASTERIX_EXTERNAL_DEV_HOME}/src/main/resources
 echo ASTERIX_EXTERNAL_DEV_RESOURCES = ${ASTERIX_EXTERNAL_DEV_RESOURCES}
 echo ASTERIX_SOURCE = ${ASTERIX_SOURCE}
 echo "MAVEN = mvn clean && mvn ${ONLINE} package -DskipTests"
+echo VERSION = ${VERSION}
+#exit 1
 
 ${MANAGIX_HOME}/bin/managix stop -n a1
 ${MANAGIX_HOME}/bin/managix delete -n a1
@@ -90,13 +106,13 @@ cd ${ASTERIX_SOURCE}
 mvn clean && mvn ${ONLINE} package -DskipTests
 
 echo "Installing new Asterix Copy"
-cp ${ASTERIX_SOURCE}/asterix-installer/target/asterix-installer-0.8.6-SNAPSHOT-binary-assembly.zip ${MANAGIX_HOME}/
+cp ${ASTERIX_SOURCE}/asterix-installer/target/asterix-installer-${VERSION}-SNAPSHOT-binary-assembly.zip ${MANAGIX_HOME}/
 cd ${MANAGIX_HOME}
-unzip asterix-installer-0.8.6-SNAPSHOT-binary-assembly.zip
+unzip asterix-installer-${VERSION}-SNAPSHOT-binary-assembly.zip
 cp ${ASTERIX_EXTERNAL_DEV_RESOURCES}/reset_a1.sh ./
 ${MANAGIX_HOME}/bin/managix configure
 cp ${ASTERIX_EXTERNAL_DEV_RESOURCES}/AsterixManagement/asterix-configuration.xml ./conf
 cp ${ASTERIX_EXTERNAL_DEV_RESOURCES}/AsterixManagement/local.xml ./clusters/local/
 
 echo "Cleaning up"
-rm asterix-installer-0.8.6-SNAPSHOT-binary-assembly.zip
+rm asterix-installer-${VERSION}-SNAPSHOT-binary-assembly.zip
